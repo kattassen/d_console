77190438cbca44db4e4efd40bf0ba948cf08d8a2
Simon <x@x.com>
2015-10-14 15:01:05 +0200

    Initial commit.

---
+++ console.py
@@ -0,0 +1,61 @@
+import time
+
+DBG_LOG = True
+
+class ColorButton:
+    """Class for handling of colorful buttons"""
+
+    def __init__(self, color, gpio_pin):
+        self.color = color
+        self.pin = gpio_pin
+        self.status = False
+
+    def getStatus(self):
+        return self.status
+
+class ButtonLed:
+    """Class for handling of colorful led lights on the console"""
+    
+    def __init__(self, color, gpio_pin):
+        self.color = color
+        self.pin = gpio_pin
+        self.status = False
+
+    def setStatus(self, status):
+        self.status = status
+
+def main():
+    """Main function"""
+
+    colorDict = {"red" : 0,
+                 "blue" : 0,
+                 "green" : 0,
+                 "yellow" : 0}
+
+    btnList = [ColorButton("red", 11),
+               ColorButton("blue", 12),
+               ColorButton("green", 12),
+               ColorButton("yellow", 12),
+               ColorButton("white", 12)]
+
+    ledList = [ButtonLed("red", 11),
+               ButtonLed("blue", 12),
+               ButtonLed("green", 12),
+               ButtonLed("yellow", 12)]
+        
+    while(True):
+        time.sleep(1)
+
+        # Check all buttons and store values
+        for btn in btnList:
+            colorDict[btn.color] = btn.getStatus()
+
+        if DBG_LOG:
+            print "Buttons satus: " + str(colorDict)
+
+        # Set LED according to button status
+        for led in ledList:
+            led.setStatus(colorDict[btn.color])
+        
+if __name__ == "__main__":
+    main()
